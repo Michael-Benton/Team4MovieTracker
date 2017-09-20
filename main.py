d4828b7dfb4f22551027048a7261639a2c681c59
Michael Benton <michaelbenton@Michaels-MacBook-Pro.local>
2017-09-19 20:17:12 -0400

    Second commit. Changing to see if it works

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask
+import pdb
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello World"
+    #pdb.set_trace()
+    return "hello"
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run()
