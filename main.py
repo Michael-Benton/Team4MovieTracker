f8eea623f08e3a6a35c6b2af31f286b21c050295
michaelbenton <michael.benton@knights.ucf.edu>
2017-11-04 22:42:15 -0400

    Full functionality of website. Now all we have to do is change the way it looks. Full Functionality includes: -Login -Register -A User can only register with one email(Unique) -After logging in, profile page comes up. -After logging out, login page comes up. -After registering, profile page comes up.

--- main.py
+++ main.py
@@ -1,43 +1,80 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import render_template, url_for, redirect, request
-
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+from flask_mail import Mail
+from flask_security.forms import RegisterForm, StringField, Required
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://michaelbenton@localhost/flaskmovie'
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECURITY_PASSWORD_SALT'] = b"xxx"
+app.config['SECURITY_PASSWORD_HASH'] = "sha512_crypt"
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'bill.haverty1234@gmail.com'
+app.config['MAIL_PASSWORD'] = 'golden10'
+app.config['SECURITY_EMAIL_SENDER'] = 'no-reply@localhost'
+app.config['SECURITY_POST_LOGIN_VIEW'] = 'profile/<email>'
+app.config['SECURITY_POST_LOGOUT_VIEW'] = 'login'
+app.config['SECURITY_POST_REGISTER_VIEW'] = 'profile/<email>'
+mail = Mail(app)
+
 db = SQLAlchemy(app)
 
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
-class User(db.Model):
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(40), unique=False)
-    last_name = db.Column(db.String(40), unique=False)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(140), unique=True)
-
-    def __init__(self, first_name, last_name, username, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.username = username
-        self.password = password
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(120))
+    email = db.Column(db.String(180), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,backref=db.backref('users', lazy='dynamic'))
 
 
-@app.route('/login')
+class ExtendedRegisterForm(RegisterForm):
+    first_name = StringField('First Name', [Required()])
+    last_name = StringField('Last Name', [Required()])
+
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore, register_form=ExtendedRegisterForm)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('login.html')
+    error = None
+    return render_template('index.html', error=error)
 
 
-@app.route('/create_account')
-def create_account():
-    return render_template('create_account.html')
+@app.route('/profile/<email>')
+@login_required
+def profile(email):
+    user = User.query.filter_by(email=email).first()
+    return render_template('profile.html', user=user)
 
 
 @app.route('/post_user', methods=['POST'])
 def post_user():
-    user = User(request.form['first_name'], request.form['last_name'], request.form['username'], request.form['password'])
+    user = User(request.form['first_name'], request.form['last_name'], request.form['email'], request.form['password'])
     db.session.add(user)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
