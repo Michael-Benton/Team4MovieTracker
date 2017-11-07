5af6adb119600407302c873b886ec3c3600636b2
michaelbenton <michael.benton@knights.ucf.edu>
2017-11-07 09:01:33 -0500

    Added Separate User Functionality. - Now a user has to have admin priv. to add movies/tv shows to the database - changed the database columns so make sure you delete the old user table and then create the new one before doing anything. See Readme

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask import render_template, url_for, redirect, request
+from flask import render_template, url_for, redirect, request, flash, abort, get_flashed_messages
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 from flask_mail import Mail
 from flask_security.forms import RegisterForm, StringField, Required
+from flask_login import current_user, LoginManager
+#import flask_whooshalchemy as wa
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://michaelbenton@localhost/flaskmovie'
@@ -18,11 +20,12 @@ app.config['MAIL_USERNAME'] = 'bill.haverty1234@gmail.com'
 app.config['MAIL_PASSWORD'] = 'golden10'
 app.config['SECURITY_EMAIL_SENDER'] = 'no-reply@localhost'
 app.config['SECURITY_POST_LOGIN_VIEW'] = 'profile/<email>'
-app.config['SECURITY_POST_LOGOUT_VIEW'] = 'login'
+app.config['SECURITY_POST_LOGOUT_VIEW'] = 'index'
 app.config['SECURITY_POST_REGISTER_VIEW'] = 'profile/<email>'
 mail = Mail(app)
 
 db = SQLAlchemy(app)
+login_manager = LoginManager()
 
 roles_users = db.Table('roles_users',
         db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
@@ -41,9 +44,27 @@ class User(db.Model, UserMixin):
     last_name = db.Column(db.String(120))
     email = db.Column(db.String(180), unique=True)
     password = db.Column(db.String(255))
+    is_admin = db.Column(db.Boolean(), default=False)
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,backref=db.backref('users', lazy='dynamic'))
+
+
+
+
+
+class MovieTV(db.Model):
+    #__searchable__ = ['title', 'producer', 'description']
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), unique=True)
+    releaseDate = db.Column(db.Integer)
+    producer = db.Column(db.String(100))
+    description = db.Column(db.String(300), unique=True)
+    genre = db.Column(db.String(50))
+
+
+#wa.whoosh_index(app, MovieTV)
 
 
 class ExtendedRegisterForm(RegisterForm):
@@ -56,10 +77,28 @@ user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore, register_form=ExtendedRegisterForm)
 
 
+@login_manager.user_loader
+def load_user(id):
+        return User.query.get(int(id))
+
+
+def require_admin():
+    if current_user.is_admin == False:
+        abort(403)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     error = None
     return render_template('index.html', error=error)
+
+
+@app.route('/addMovie', methods=['GET', 'POST'])
+@login_required
+def addMovie():
+    require_admin()
+    error = None
+    return render_template('addMovie.html', error=error)
 
 
 @app.route('/profile/<email>')
@@ -75,6 +114,14 @@ def post_user():
     db.session.add(user)
     db.session.commit()
     return redirect(url_for('login'))
+
+
+@app.route('/post_movieTVShow', methods=['POST'])
+def post_movieTVShow():
+    newItem = MovieTV(request.form['title'], request.form['releaseDate'], request.form['producer'], request.form['description'], request.form['genre'])
+    db.session.add(newItem)
+    db.session.commit()
+    return redirect(url_for('profile/<email>'))
 
 
 if __name__ == '__main__':
