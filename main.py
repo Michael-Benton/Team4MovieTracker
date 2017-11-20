891623b064175a2b31bbc1e1dfd9b28a1c01ddaf
michaelbenton <michael.benton@knights.ucf.edu>
2017-11-20 16:52:35 -0500

    Added function "Add to WatchList" - Users can now add movies to watch list if they're logged in - Movies Display on users profile

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, request, abort
+from flask import Flask, render_template, url_for, redirect, request, abort, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 from flask_mail import Mail
@@ -6,7 +6,7 @@ from flask_security.forms import RegisterForm, StringField, Required
 from flask_login import current_user, LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Silvertigger97!@localhost:5432/flaskmovie'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://michaelbenton@localhost:5432/flaskmovie'
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['SECURITY_REGISTERABLE'] = True
 app.config['SECURITY_PASSWORD_SALT'] = b"xxx"
@@ -17,9 +17,9 @@ app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'bill.haverty1234@gmail.com'
 app.config['MAIL_PASSWORD'] = 'abcd1234!@'
 app.config['SECURITY_EMAIL_SENDER'] = 'no-reply@localhost'
-app.config['SECURITY_POST_LOGIN_VIEW'] = 'profile/<email>'
+app.config['SECURITY_POST_LOGIN_VIEW'] = 'index'
 app.config['SECURITY_POST_LOGOUT_VIEW'] = 'index'
-app.config['SECURITY_POST_REGISTER_VIEW'] = 'profile/<email>'
+app.config['SECURITY_POST_REGISTER_VIEW'] = 'index'
 mail = Mail(app)
 
 db = SQLAlchemy(app)
@@ -30,6 +30,16 @@ roles_users = db.Table('roles_users',
                        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
 
+@app.errorhandler(404)
+def pageNotFound(error):
+    return "page not found"
+
+
+@app.errorhandler(500)
+def duplicateFound(error):
+    return "Duplicate found in database. Try again"
+
+
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
@@ -37,7 +47,8 @@ class Role(db.Model, RoleMixin):
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     first_name = db.Column(db.String(120))
     last_name = db.Column(db.String(120))
     email = db.Column(db.String(180), unique=True)
@@ -48,9 +59,27 @@ class User(db.Model, UserMixin):
     roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
 
 
+class watchList(db.Model):
+    __tablename__ = "watchList"
+
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('Movie.movie_id'))
+    tv_id = db.Column(db.Integer, db.ForeignKey('TV.tv_id'))
+    title = db.Column(db.String(200), unique=True)
+    releaseDate = db.Column(db.Date)
+    producer = db.Column(db.String(100))
+    description = db.Column(db.String(300))
+    genre = db.Column(db.String(50))
+    image = db.Column(db.String(300))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship("User", backref=db.backref('watchList'))
+
+
+
 class Movie(db.Model):
     __tablename__ = 'Movie'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    movie_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), unique=True)
     releaseDate = db.Column(db.Date)
     producer = db.Column(db.String(100))
@@ -58,12 +87,13 @@ class Movie(db.Model):
     genre = db.Column(db.String(50))
     image = db.Column(db.String(300))
     is_movie = db.Column(db.Boolean(), default=True)
-    is_tvshow = db.Column(db.Boolean(), default=False)
+    is_show = db.Column(db.Boolean(), default=False)
 
 
 class TV(db.Model):
     __tablename__ = 'TV'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    tv_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(200), unique=True)
     releaseDateTime = db.Column(db.DateTime)
     producer = db.Column(db.String(100))
@@ -71,7 +101,7 @@ class TV(db.Model):
     genre = db.Column(db.String(50))
     image = db.Column(db.String(300))
     is_movie = db.Column(db.Boolean(), default=False)
-    is_tvshow = db.Column(db.Boolean(), default=True)
+    is_show = db.Column(db.Boolean(), default=True)
 
 
 class ExtendedRegisterForm(RegisterForm):
@@ -116,49 +146,137 @@ def addTV():
 
 @app.route('/search', methods=["GET"])
 def search():
+    error = None
     user_input = request.args.get("query")
     search_results_movie = Movie.query.all()
     search_results_tv = TV.query.all()
     i = 0
     j = 0
     listOfResults = []
+
     while j < len(search_results_movie):
-        if search_results_movie[i].title.lower() == user_input.lower() or \
-           search_results_movie[i].producer.lower() == user_input.lower() or \
-           search_results_movie[i].genre.lower() == user_input.lower():
-            print(search_results_movie[i].title)
+        if (search_results_movie[i].title.lower()).find(user_input.lower()) is not -1:
             listOfResults.append(search_results_movie[i])
+
         i += 1
         j += 1
-    return render_template("index.html", movies=listOfMovies)	
+
+    i = 0
+    j = 0
+
+    while j < len(search_results_tv):
+        if (search_results_tv[i].title.lower()).find(user_input.lower()) is not -1:
+            listOfResults.append(search_results_tv[i])
+        i += 1
+        j += 1
+
+    if len(listOfResults) is 0:
+        error = 'No results found'
+        return render_template("index.html", error=error)
+
+    return render_template("index.html", results=listOfResults)
 
 
 @app.route('/getRecommendations', methods=["GET"])
 def getRecommendations():
     user_watchList = getUserWatchList()
-	last_entry = user_watchList[-1]
-	last_entry_genre = last_entry.genre
-	all_movies = MovieTV.query.all()
-	recommendationList =[] 
-	for movie in all_movies:
-		if movie.genre == last_entry_genre:
-			recommendationList.append(movie)
-		if (len(recommendationList) == 5):
-			break
-	return rend_template("index.html", recommendations=recommendationList)
+    last_entry = user_watchList[-1]
+    last_entry_genre = last_entry.genre
+    all_movies = Movie.query.all()
+    recommendationList =[]
+    for movie in all_movies:
+        if movie.genre == last_entry_genre:
+            recommendationList.append(movie)
+        if (len(recommendationList) == 5):
+            break
+    return render_template("index.html", recommendations=recommendationList)
 
+
+@app.route('/addToWatchList', methods=["POST"])
+@login_required
+def addToWatchList():
+    error=None
+    movie_list = Movie.query.all()
+    tv_list = TV.query.all()
+    movie = None
+    show = None
+    i = 0
+    j = 0
+
+    while j < len(movie_list):
+        if int(movie_list[i].movie_id) == int(request.form.get('result')):
+            movie = movie_list[i]
+
+        i += 1
+        j += 1
+
+    i = 0
+    j = 0
+
+    while j < len(tv_list):
+        if int(tv_list[i].tv_id) == int(request.form.get('result')):
+            show = tv_list[i]
+        else:
+            show = None
+
+        i += 1
+        j += 1
+
+    if show is None:
+
+        if db.session.query(watchList).filter(watchList.movie_id == movie.movie_id, watchList.user_id == current_user.id).first():
+            error = 'Movie is already in your watchlist'
+            return redirect(url_for('MovieDescription', title=movie.title, error=error))
+
+        print(movie.title)
+        movieItem = watchList(user_id=current_user.id,movie_id=movie.movie_id, tv_id=None, title=movie.title, releaseDate=movie.releaseDate, producer=movie.producer, description=movie.description, genre=movie.genre, image=movie.image)
+        db.session.add(movieItem)
+        db.session.commit()
+
+        flash("This Movie has been added to your watch list!")
+        return redirect(url_for('profile', id=current_user.id, movies=watchList.query.all()))
+
+    else:
+        if db.session.query(watchList).filter(watchList.tv_id == show.tv_id,
+                                              watchList.user_id == current_user.id).first():
+            error = 'TV Show is already in your watchlist'
+            return redirect(url_for('TVShowDescription', title=show.title, error=error))
+
+        showItem = watchList(user_id=current_user.id, movie_id=None, tv_id=show.tv_id,title=show.title, releaseDate=show.releaseDate, producer=show.producer, description=show.description, genre=show.genre, image=show.image)
+        db.session.add(showItem)
+        db.session.commit()
+
+        flash("This TV Show has been added to your watch list!")
+        return redirect(url_for('profile', id=current_user.id, movies=watchList.query.all()))
+
+
+@app.route('/deleteFromWatchList', methods=['POST'])
+def deleteFromWatchList():
+    deletedItem = watchList.query.filter_by(user_id=current_user.id, id=request.form.get('movie.movie_id'))
+    db.session.delete(deletedItem)
+    db.commit()
 
 @app.route('/getUserWatchList', methods=["GET"])
 def getUserWatchList():
-	user_watchList = []
-	
-	return user_watchList
-@app.route('/profile/<email>')
+    user_watchList = []
+
+    return user_watchList
+
+
+@app.route('/profile/<id>')
 @login_required
-def profile(email):
-    user = User.query.filter_by(email=email).first()
-    search_results_movies = Movie.query.all()   # This will be changed to accommodate the watchlist eventually, don't worry about it not displaying TV shows.
-    return render_template('profile.html', user=user, movies=search_results_movies)
+def profile(id):
+    user = User.query.filter_by(id=id).first()
+    search_results_movies = watchList.query.all()
+    listItems = []
+    i = 0
+
+    while i < len(search_results_movies):
+        if int(search_results_movies[i].user_id) == int(id):
+            listItems.append(search_results_movies[i])
+        i += 1
+
+    return render_template('profile.html', movies=listItems)
 
 
 @app.route('/post_user', methods=['POST'])
@@ -169,26 +287,30 @@ def post_user():
     return redirect(url_for('login'))
 
 
-@app.route('/MovieTVShowDescription/<string:title>')
-def MovieTVShowDescription(title):
-    movieItems = Movie.query.all()
+@app.route('/TVShowDescription/<string:title>')
+def TVShowDescription(title):
     tvItems = TV.query.all()
-    i = 0
-    j = 0
-
-    while j < len(movieItems):
-        if movieItems[i].title == title:
-            print(movieItems[i].title)
-            return render_template("MovieTVShowDescription.html", result=movieItems[i])
-        i+= 1
-        j+= 1
-
     i = 0
     j = 0
 
     while j < len(tvItems):
         if tvItems[i].title == title:
-            return render_template("MovieTVShowDescription.html", result=tvItems[i])
+            return render_template("TVShowDescription.html", result=tvItems[i])
+        i += 1
+        j += 1
+
+    return redirect(url_for('index'))
+
+
+@app.route('/MovieDescription/<string:title>')
+def MovieDescription(title):
+    movieItems = Movie.query.all()
+    i = 0
+    j = 0
+
+    while j < len(movieItems):
+        if movieItems[i].title == title:
+            return render_template("MovieDescription.html", result=movieItems[i])
         i += 1
         j += 1
 
@@ -197,12 +319,13 @@ def MovieTVShowDescription(title):
 
 @app.route('/post_Movie', methods=['POST'])
 def post_Movie():
-    newItem = Movie(title=request.form['title'], releaseDate=request.form['releaseDate'],
+    newItem = Movie(title=(request.form['title']).upper(), releaseDate=request.form['releaseDate'],
                       producer=request.form['producer'], description=request.form['description'],
                       genre=request.form['genre'], image=request.form['image'])
     db.session.add(newItem)
     db.session.commit()
     return redirect(url_for('index'))
+
 
 @app.route('/post_TVShow', methods=['POST'])
 def post_TVShow():
@@ -212,6 +335,7 @@ def post_TVShow():
     db.session.add(newItem)
     db.session.commit()
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
