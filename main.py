bb6250f76e1fd89e54537d4fb8f30e58f68a5995
LoganTHarvell <logan@harvell.us>
2017-11-28 14:38:50 -0500

    json encoding and API support

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for, redirect, request, abort, flash, session
+from flask import Flask, render_template, url_for, redirect, request, abort, flash, session, json
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 from flask_mail import Mail
 from flask_security.forms import RegisterForm, StringField, Required
 from flask_login import current_user, LoginManager
+from sqlalchemy.ext.declarative import DeclarativeMeta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Silvertigger97!@localhost:5432/flaskmovie'
@@ -21,6 +22,7 @@ app.config['SECURITY_EMAIL_SENDER'] = 'no-reply@localhost'
 app.config['SECURITY_POST_LOGIN_VIEW'] = 'index'
 app.config['SECURITY_POST_LOGOUT_VIEW'] = 'index'
 app.config['SECURITY_POST_REGISTER_VIEW'] = 'index'
+app.config['WTF_CSRF_ENABLED'] = False
 mail = Mail(app)
 
 db = SQLAlchemy(app)
@@ -408,6 +410,23 @@ def movieDuplicate():
 def showDuplicate():
     return render_template("showDuplicateFound.html")
 
+
+class AlchemyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            # an SQLAlchemy class
+            fields = {}
+            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                data = obj.__getattribute__(field)
+                try:
+                    json.dumps(data) # this will fail on non-encodable values, like other classes
+                    fields[field] = data
+                except TypeError:
+                    fields[field] = None
+            # a json-encodable dict
+            return fields
+
+        return json.JSONEncoder.default(self, obj)
 
 def test_new_user():
     try:
